07e5ccece2ff4950cbd13f66248bb6901df57f69
Olaf Slomp <git@oslomp.nl>
2025-12-08 18:08:48 +0100

    Day 8a completed

--- src/day8.rs
+++ src/day8.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::{collections::HashMap, iter};
+use std::{collections::HashMap};
 
 #[derive(Debug)]
 pub struct ElectricalBox(isize, isize, isize);
@@ -32,8 +32,14 @@ pub fn calculate_distances(boxes: Vec<ElectricalBox>) -> HashMap<(usize, usize),
     distances_memoization
 }
 
-pub fn is_valid_circuit_combinations((circuit_a, circuit_b): &(&Vec<usize>, &Vec<usize>), valid_id_pairs: &Vec<&(usize, usize)>) -> bool {
-    circuit_a.iter().cartesian_product(circuit_b.iter()).any(|(x, y)| valid_id_pairs.contains(&&(*x, *y)))
+pub fn is_valid_circuit_combinations(
+    (circuit_a, circuit_b): &(&Vec<usize>, &Vec<usize>),
+    valid_id_pairs: &Vec<&(usize, usize)>,
+) -> bool {
+    circuit_a
+        .iter()
+        .cartesian_product(circuit_b.iter())
+        .any(|(x, y)| valid_id_pairs.contains(&&(*x, *y)))
 }
 
 pub fn main(input: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
@@ -41,8 +47,7 @@ pub fn main(input: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
         .into_iter()
         .map(ElectricalBox::new)
         .collect::<Vec<ElectricalBox>>();
-    let mut circuits: Vec<Vec<usize>> =
-        boxes.iter().enumerate().map(|(idx, _)| vec![idx]).collect();
+    let mut circuits: Vec<Vec<usize>> = Vec::new();
     let iterations = match boxes.len() {
         20 => 10,
         1000 => 1000,
@@ -50,24 +55,34 @@ pub fn main(input: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let mut distances_memoization = calculate_distances(boxes);
-    let last_distance = distances_memoization.values().sorted().take(iterations).last().unwrap().clone();
-    distances_memoization.retain(|_key, value| *value <= last_distance );
+    let last_distance = distances_memoization
+        .values()
+        .sorted()
+        .take(iterations)
+        .last()
+        .unwrap()
+        .clone();
+    distances_memoization.retain(|_key, value| *value <= last_distance);
     let valid_pairs = distances_memoization.keys().collect_vec();
-
-    println!("{:?}", valid_pairs);
-    println!("Calculated distances.");
 
     for (i, j) in valid_pairs {
         println!("{:?}", circuits);
         println!("{:?}, {:?}", i, j);
-        let Some(position_box_a) = circuits
-            .iter()
-            .position(|x| x.contains(i)) else {continue;};
-        let mut circuit_a = circuits.remove(position_box_a);
-        let Some(position_box_b) = circuits
-            .iter()
-            .position(|x| x.contains(j)) else {continue;};
-        let mut circuit_b = circuits.remove(position_box_b);
+
+        let mut circuit_a = match circuits.iter().position(|x| x.contains(i)) {
+            Some(x) => circuits.remove(x),
+            None => vec![i.clone()],
+        };
+
+        if circuit_a.contains(j) {
+            circuits.push(circuit_a);
+            continue;
+        }
+
+        let mut circuit_b = match circuits.iter().position(|x| x.contains(j)) {
+            Some(x) => circuits.remove(x),
+            None => vec![j.clone()],
+        };
         let _ = circuit_a.append(&mut circuit_b);
 
         circuits.push(circuit_a);
