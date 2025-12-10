3283648ff5e40b4622fdda71cf26d67d00d95cc9
Olaf Slomp <git@oslomp.nl>
2025-12-11 00:59:43 +0100

    WIP

--- src/day9.rs
+++ src/day9.rs
@@ -1,38 +1,106 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
-type Idxf = (f64, f64);
-type Idx = (isize, isize);
+type Id = (isize, isize);
 
-pub fn checker(idx: &Idxf, edge: &Vec<Idxf>) -> bool {
-    let count = edge
-        .iter()
-        .tuple_windows()
-        .filter(|(a, _)| idx.0 >= a.0)
-        .map(|(a, b)| {
-            if a.1 < b.1 {
-                (a.1 + 0.5, b.1 - 0.5)
-            } else {
-                (b.1 + 0.5, a.1 - 0.5)
+pub fn create_grid(edges: &Vec<Id>) -> HashMap<Id, bool> {
+    let mut grid = HashMap::new();
+    let max = 12;
+    for i in 0..max {
+        for j in 0..max {
+            grid.insert((i, j), false);
+        }
+    }
+
+    let windows = edges.iter().tuple_windows().collect_vec();
+
+    for (a, b) in windows {
+        if a.0 == b.0 {
+            for i in a.1.min(b.1)..=a.1.max(b.1) {
+                grid.insert((a.0, i), true);
             }
-        })
-        .filter(|(a, b)| *a < idx.1 && idx.1 < *b)
-        .count();
+        } else {
+            for i in a.0.min(b.0)..=a.0.max(b.0) {
+                grid.insert((i, a.1), true);
+            }
+        }
+    }
+    for i in edges[0].0..=edges[edges.len() - 1].1 {
+        grid.insert((edges[0].0, i), true);
+    }
+    for i in edges[0].1..=edges[edges.len() - 1].0 {
+        grid.insert((i, edges[0].1), true);
+    }
 
-    count % 2 == 0
+    for i in 0..max {
+        println!("");
+        for j in 0..max {
+            print!("{}", match grid.get(&(i, j)).unwrap_or(&false) {
+                true => "#",
+                false => ".",
+            });
+        }
+    }
+
+
+    for i in 0..max {
+        let mut inside = false;
+        let mut total_inside_adj = 0;
+        for j in 0..max {
+            if inside {
+                if *grid.get(&(i-1, j)).unwrap_or(&false) && *grid.get(&(i+1, j)).unwrap_or(&false) {
+                    inside = !inside;
+                }
+                if *grid.get(&(i, j-1)).unwrap_or(&false) && *grid.get(&(i, j+1)).unwrap_or(&false) {
+            } else {
+
+
+            }
+            } && *grid.get(&(i, j)).unwrap() {
+                inside = !inside;
+            }
+            if inside && *grid.get(&(i, j-1)).unwrap_or(&false) {
+                total_inside_adj += 1;
+            }
+            if *grid.get(&(i, j)).unwrap() {
+                inside = !inside;
+            }
+
+            if inside && total_inside_adj % 2 == 0 {
+                grid.insert((i, j), true);
+            }
+        }
+    }
+
+    for i in 0..max {
+        println!("");
+        for j in 0..max {
+            print!("{}", match grid.get(&(i, j)).unwrap_or(&false) {
+                true => "#",
+                false => ".",
+            });
+        }
+    }
+    grid
 }
 
-pub fn valid_combination(a: &Idx, b: &Idx, edges: &Vec<Idxf>) -> bool {
-    let x = if a.0 > b.0 { b.0..=a.0 } else { a.0..=b.0 };
-    let y = if a.1 > b.1 { b.1..=a.1 } else { a.1..=b.1 };
-    let check = x
-        .cartesian_product(y)
-        .all(|(i, j)| checker(&(i as f64, j as f64), edges));
-    println!("({:?}, {:?}), {:?} {:?}", a, b, area(a, b), check);
-    check
-}
-
-pub fn area(a: &Idx, b: &Idx) -> usize {
+pub fn area(a: &Id, b: &Id) -> usize {
     (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1)
+}
+
+pub fn check_edge(a: &Id, b: &Id, grid: &HashMap<Id, bool>) -> bool {
+    let min_x = a.0.min(b.0);
+    let max_x = a.0.max(b.0);
+    let min_y = a.1.min(b.1);
+    let max_y = a.1.max(b.1);
+    let mut edges = (min_x..=max_x).map(|x| (x, min_y)).collect_vec();
+    edges.extend((min_x..=max_x).map(|x| (x, max_y)).collect_vec());
+    edges.extend((min_y..=max_y).map(|y| (min_x, y)).collect_vec());
+    edges.extend((min_y..=max_y).map(|y| (max_x, y)).collect_vec());
+    let edges = edges.iter().sorted().dedup().collect_vec();
+    // println!("{:?}", edges);
+    edges.iter().all(|(x, y)| *grid.get(&(*x, *y)).unwrap())
 }
 
 pub fn main(input: Vec<String>) {
@@ -43,18 +111,21 @@ pub fn main(input: Vec<String>) {
             b.parse::<isize>().expect("Not Parsable"),
         )
     });
-    let edges: Vec<Idxf> = tiles
-        .clone()
-        .map(|(a, b)| (a as f64, b as f64))
-        .collect_vec();
+    let edges: Vec<Id> = tiles.clone().collect_vec();
+    let grid = create_grid(&edges);
+
+    // println!("{:?}", grid);
 
     let a = tiles
         .tuple_combinations()
-        //.map(|(a, b)| order_rectangle(a, b))
         .sorted_by(|(a, b), (c, d)| area(&a, &b).cmp(&area(&c, &d)).reverse())
-        .filter(|(a, b)| valid_combination(a, b, &edges))
+        .filter(|(a, b)| {
+            // println!("{:?}, {:?}", a, b);
+            let bla = check_edge(a, b, &grid);
+            // println!("{:?}", bla);
+            bla
+        })
         .next()
         .unwrap();
-    //.collect_vec();
-    println!("{:?},{:?} -> {:?}", a.0, a.1, area(&a.0, &a.1));
+    // println!("{:?},{:?} -> {:?}", a.0, a.1, area(&a.0, &a.1));
 }
